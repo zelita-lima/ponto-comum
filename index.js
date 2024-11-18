4b9488c99f0ce4fcb1b85ba93a06624cedd7d914
Ana Luiza <analluizafr@gmail.com>
2024-11-18 12:27:33 -0300

    primeiras mudanças

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+var locations = [
+	['<strong>Atelier Riedel</strong><br>Raiffeisenstr. 10a<br>41751 Viersen', -15.6414507, -47.7876177, 1]
+  ];
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+	zoom: 15,
+	center: new google.maps.LatLng(-15.6414507, -47.7876177),
+	mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  var infowindow = new google.maps.InfoWindow();
+
+  var marker, i;
+
+  for (i = 0; i < locations.length; i++) {  
+	marker = new google.maps.Marker({
+	  position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+	  map: map
+	});
+
+	google.maps.event.addListener(marker, 'click', (function(marker, i) {
+	  return function() {
+		infowindow.setContent(locations[i][0]);
+		infowindow.open(map, marker);
+	  }
+	})(marker, i));
+  }
+
 // Register Service worker to control making site work offline
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker
